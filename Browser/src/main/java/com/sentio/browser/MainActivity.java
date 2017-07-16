ecd17789f9dc28c413de23589be4859bc8967e82
Quan Vu <nongdenchet8435848@gmail.com>
2017-07-16 22:49:21 +0700

    Test browser

--- Browser/src/main/java/com/sentio/browser/MainActivity.java
+++ Browser/src/main/java/com/sentio/browser/MainActivity.java
@@ -2,6 +2,7 @@ package com.sentio.browser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, getString(R.string.app_name), Toast.LENGTH_SHORT).show();
     }
 }
